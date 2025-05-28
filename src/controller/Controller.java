3aa642943d0cc463909f19b51d1ddacac2b453ca
Said Halilovic <saidh.@MacBook-Pro-von-Said.local>
2025-05-28 20:10:43 +0200

    hi

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,10 +1,16 @@
 package controller;
 
-import data.DBConnect;
 import view.View;
 
 public class Controller {
     public static void main(String[] args) {
         View.eingabe();
+        View.eingabeInt();
+        if (View.eingabeBoolean()) {
+            View.ausgabe("True wurde ausgegeben");
+        } else {
+            View.ausgabe("False wurde ausgegeben");
+        }
+
     }
 }
